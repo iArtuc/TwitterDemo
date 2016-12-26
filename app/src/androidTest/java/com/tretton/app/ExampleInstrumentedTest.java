b997315924559ce1589450f440294e82b6e07606
ilkinartuc <ilkinartuc@gmail.com>
2016-12-26 20:45:21 +0300

    Espresso ui test

--- app/src/androidTest/java/com/tretton/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tretton/app/ExampleInstrumentedTest.java
@@ -2,12 +2,20 @@ package com.tretton.app;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.tretton.app.flows.mainscreen.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -17,12 +25,29 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest
 {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception
     {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.tretton.app.debug", appContext.getPackageName());
+    }
 
-        assertEquals("com.tretton.app", appContext.getPackageName());
+    @Test
+    public void clickOnItem()
+    {
+        onView(withId(R.id.rv_activity_main_tweet_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(9, click()));
+    }
+
+    @Test
+    public void checkListItemCountIsTwenty()
+    {
+        onView(withId(R.id.rv_activity_main_tweet_list)).check(new RecyclerViewItemCountAssertion
+                (20));
     }
 }
